69730fc11f56405f529f772c1596bb02a9ed8eb3
Joel Wembo <joelwembo@outlook.com>
2024-05-07 04:05:05 +0800

    updated status

--- deployment/commands/deploy-aws-eks-cluster.sh
+++ deployment/commands/deploy-aws-eks-cluster.sh
@@ -8,17 +8,12 @@ DEPLOYMENT_NAME="prodxcloud-store"
 SERVICE_NAME="prodxcloud-cluster"
 EKS_CLUSTER_NAME="prodxcloud-cluster"
 PORT=8585
-
 # Set KUBECONFIG environment variable
 export KUBECONFIG="$KUBECONFIG"
-
 aws eks --region us-east-1 update-kubeconfig --name $EKS_CLUSTER_NAME
-
 # Apply Kubernetes manifests
 kubectl apply -f deployments/k8s/deployment.yaml
-
 # Update the Docker image in the deployment
 kubectl set image deployment/$DEPLOYMENT_NAME $DEPLOYMENT_NAME=$DOCKER_IMAGE -n $NAMESPACE
-
 # Expose the deployment as a service
 kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=$PORT  --target-port=$PORT -n $NAMESPACE --name=$SERVICE_NAME
