ace966d3cf8c620639e83767ffb3e5cf85f76abe
Joel Wembo <joelwembo@outlook.com>
2024-05-14 02:24:53 +0800

    removed secrets

--- deployment/terraform/terraform-aws-ec2-s3/vault-installer.sh
+++ deployment/terraform/terraform-aws-ec2-s3/vault-installer.sh
@@ -24,3 +24,4 @@ gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vault --assume-yes
 
+##
